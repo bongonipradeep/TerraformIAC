10cc67b6d107493ff65192e896b4d2bc8f53d6d7
bongonipradeep <119953250+bongonipradeep@users.noreply.github.com>
2023-03-23 16:11:07 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA2NLRYT74N2UHKQ4E"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 
   
 }
